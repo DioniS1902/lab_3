e097b4f838a3bd133316f817fa67ec68f101c9ce
Pitkov Denis <den70007.ua@gmail.com>
2021-03-07 19:49:02 +0200

    written code based on UML table

---
+++ savana/manager/savanna_manager.py
@@ -0,0 +1,23 @@
+from ..animal import *
+
+
+class SortOrder(Enum):
+    ASC = 0
+    DESC = 1
+
+
+class SavannaManager:
+    def __init__(self):
+        self.animals = []
+
+    def add_animal(self, animal: Animal):
+        self.animals.append(Animal)
+
+    def add_animals(self, animals: list):
+        self.animals += animals
+
+    def find_predators(self, min_number_of_meat: int):
+        return [animal for animal in self.animals if animal > min_number_of_meat and animal.type_of_food_used is FoodUsed.PREDATOR]
+
+    def sort_by_weight_of_food_consumed(self, animals: list, order: SortOrder):
+        return sorted(animals, key=lambda x: x.number_of_food, reverse=order.value)
